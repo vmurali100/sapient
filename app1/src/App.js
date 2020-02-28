1d090f81e285eeaa15d1e6c5d6fd8e61d07f17e2
vmurali100 <vmurali100@gmail.com>
2020-02-28 14:26:16 +0530

    adding

--- app1/src/App.js
+++ app1/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 // import Sapient2 from "./Sapient2";
 import Register from "./Register";
+import Crud from "./CRUD/Crud";
 
 function App() {
   return (
     <div className="App">
       {/* <Sapient /> */}
       {/* <Sapient2 /> */}
-      <Register />
+      {/* <Register /> */}
+      <Crud />
     </div>
   );
 }
---
+++ app1/src/CRUD/Crud.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Crud extends Component {
+  render() {
+    return (
+      <div>
+        <h2>CRUD APP</h2>
+      </div>
+    );
+  }
+}
